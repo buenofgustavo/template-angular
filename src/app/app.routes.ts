709434d7892a083f7cb21c6dc95c0b6764664f94
gustavo.ferreira <buenofgustavo@gmail.com>
2024-06-13 15:33:33 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import path from 'path';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path: '', component: SidebarComponent,
+        children: [
+            {path: 'home',component: HomeComponent},
+        ]
+    }
+];
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,44 @@
+import { ApplicationConfig, Component, OnInit } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { NgIf } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { StyleClassModule } from 'primeng/styleclass';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [SidebarModule, ButtonModule, NgIf, RippleModule, AvatarModule, StyleClassModule,
+    MenubarModule, BadgeModule, InputTextModule, CommonModule, AvatarGroupModule
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+
+export class SidebarComponent implements OnInit {
+  
+  sidebarVisible: boolean = false;
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+    console.log(this.sidebarVisible)
+  }
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'Home',
+              icon: 'pi pi-home'
+          },
+      ];
+  }
+}
